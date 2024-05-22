8fe2828a25cd251792751b7b1612ce264bb6e899
alphadiop <alphadiop@gmail.com>
2024-05-22 23:15:45 +0200

    mise a jour

--- web_api/main.py
+++ web_api/main.py
@@ -1,13 +1,17 @@
 import json
 import requests
-
-url = "https://aoudiop.pythonanywhere.com/api/GetPizzas"
+import pandas as pd
+url = "https://alphadiop.pythonanywhere.com/api/GetPizzas"
 data = requests.get(url)
-#print(data.text)
+print(data.text)
 # deseriliser
 pizzas = json.loads(data.text)
-# print(pizzas)
+dico_pizza = [pizzaMondel['fields'] for pizzaMondel in pizzas]
+dataframe = pd.DataFrame.from_dict(dico_pizza)
+print('\n')
 
-for pizzaMondel in pizzas:
-    pizza = pizzaMondel['fields']
-    print(pizza['nom'])
+# for pizzaMondel in pizzas:
+#     pizza = pizzaMondel['fields']
+#     print(pizza['nom']+ " : " +str(pizza['prix']))
+
+print(dataframe)
